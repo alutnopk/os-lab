4436a2e08118cecb720df7b6dcd131546790fb87
nithishnani-277 <103628957+nithishnani-277@users.noreply.github.com>
2023-01-20 19:23:37 +0530

    Create Assgn1_7_28.sh

---
+++ A1/Assgn1_7_28.sh
@@ -0,0 +1,24 @@
+if [ $# -eq 0 ]; then
+  echo "Please provide an input directory path"
+  exit 1
+fi
+
+input_dir=$1
+output_dir=$2
+
+if [ ! -d "$output_dir" ]; then
+  mkdir "$output_dir"
+fi
+
+for file in "$input_dir"; do
+  while IFS= read -r line; do
+    first_letter=$(echo "$line" | head -c 1)
+    echo "$line" >> "$output_dir/$first_letter.txt"
+  done < "$file"
+done
+
+for file in "$output_dir"/; do
+  sort "$file" -o "$file"
+done
+
+echo "Names sorted and grouped by first letter in output directory"
