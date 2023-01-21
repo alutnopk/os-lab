accf6fdcdf6867ef40fbe5c4567bcbfb2d7082a4
alutnopk <kartikpontula@gmail.com>
2023-01-21 22:53:43 +0530

    idk bleh

--- A1/Assgn1_3_28.sh
+++ A1/Assgn1_3_28.sh
@@ -21,10 +21,16 @@ attributes="$@"
 for jsonl_file in "$jsonl_dir"/*.jsonl; do
   # create a corresponding CSV file with the same name in the output directory
   csv_file="$csv_dir"/$(basename "$jsonl_file" .jsonl).csv
-  attrs=$(echo $attributes | sed -r 's/ /, ./g; s/^/./')
+  attrs=$(echo $attributes | sed -r 's/ /, ./g; s/^/./') #s/$/\n/ # sed -r 's/ /,", ",./g; s/^/./'
   [ ! -e "$csv_file" ] && { touch "$csv_file"; echo "$attrs" > "$csv_file"; }
   echo $attrs
   # extract the specified attributes from each JSON object
   # cat "$jsonl_file" | jq -r '.' | jq -r '.'$attributes | sed 's/"/""/g' | awk -F, '{gsub(/"/,"\"\""); print}' | grep -v '^\s*$' | sed 's/^,//' >> "$csv_file"
-  cat "$jsonl_file" | jq -r $attrs | sed 's/"/""/g' | awk -F, '{gsub(/"/,"\"\""); print}' | grep -v '^\s*$' | sed 's/^,//' >> "$csv_file"
+  cat "$jsonl_file" | jq -c "$attrs">> "$csv_file" # 
+
+	# while IFS='\n' read -r line || [[ -n "$line" ]]; do
+	# 		echo "$line"
+	# done < "$json1_file"
+
+
 done
