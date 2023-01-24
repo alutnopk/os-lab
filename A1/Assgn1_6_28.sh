5b933b140a72907624d5e8da13ae919bae024898
Rahul Mandal <85156165+rahulmandalgg@users.noreply.github.com>
2023-01-24 20:12:02 +0530

    Update Assgn1_6_28.sh

--- A1/Assgn1_6_28.sh
+++ A1/Assgn1_6_28.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
+> output.txt
+file=$1
+for ((i=2;i<=1000;i++));do
+        arr[$i]=1
+    done
 
-while IFS= read line
+for ((i=2;i<=1000;i++));do  	
+    	if [[ ${arr[$i]} -ne 0 ]]; then
+    		  for (( j=$((i*2)); j<=1000; j+=$i )); do
+    		  	 arr[j]=0
+    		  done
+    	fi
+done
+a=2
+b=0
+for ((i=2;i<=1000;i++));do
+	if [[ ${arr[$i]} -ne 0 ]]; then
+		#echo $a
+		arr1[b]=$((a))
+		b=$((b+1))
+	fi
+	a=$((a+1))
+    done
+
+while IFS= read -r line
 do
-    num=$(echo $line | grep -o '[0-9]\+')
-    arr=()
-    arr[0]=0
-    arr[1]=0
-    for((i=2;i<=num;i++)); do
-        arr[i]=1
-    done
-    for((i=2;i*i<=num;i++)); do
-        if [ ${arr[i]} == 1 ]
-        then
-            for((j=i*i;j<=num;j+=i)); do
-                arr[j]=0
-            done
-        fi
-    done
-    for((i=2;i<=num;i++)); do
-        if [ ${arr[i]} == 1 ]
-        then
-            echo -n $i
-        fi
-    done
-    echo ""
+    num="$((${line//[ $'\001'-$'\037']}))"
+    for prime in ${arr1[@]}; do
+    	if [[ $((num%prime)) -eq 0 ]]; then
+    		echo -n $prime" " >> output.txt
+    	fi
+    done 
+    echo "" >> output.txt
+    echo "" >> output.txt
 done <"$file"
