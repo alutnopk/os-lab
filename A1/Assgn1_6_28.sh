d92f811c09d075f97bd4afd45110207ba01d37c1
Rahul Mandal <85156165+rahulmandalgg@users.noreply.github.com>
2023-01-24 16:46:15 +0530

    Create Assgn1_6_28.sh

---
+++ A1/Assgn1_6_28.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while IFS= read line
+do
+    num=$(echo $line | grep -o '[0-9]\+')
+    arr=()
+    arr[0]=0
+    arr[1]=0
+    for((i=2;i<=num;i++)); do
+        arr[i]=1
+    done
+    for((i=2;i*i<=num;i++)); do
+        if [ ${arr[i]} == 1 ]
+        then
+            for((j=i*i;j<=num;j+=i)); do
+                arr[j]=0
+            done
+        fi
+    done
+    for((i=2;i<=num;i++)); do
+        if [ ${arr[i]} == 1 ]
+        then
+            echo -n $i
+        fi
+    done
+    echo ""
+done <"$file"
