ebb5db0c1dcb28c3c05b8351d455d43a94b2ab5e
Kartik Pontula <kartikpontula@gmail.com>
2023-03-17 23:03:58 +0530

    Init.

---
+++ A5/headers.h
@@ -0,0 +1,26 @@
+#ifndef __HEADERS_H
+#define __HEADERS_H
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <queue>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <climits>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+
+
+#endif
