0c735f35d6a214d204383b6981606a8bcbcdb2ee
rahulmandalgg <rahulmandal912@gmail.com>
2023-03-19 12:56:42 +0530

    idkwhymanihatelifefuckmedaddy

--- A5/cleaner.cpp
+++ A5/cleaner.cpp
@@ -6,17 +6,6 @@ void* cleaner_routine(void* arg)
     pthread_mutex_lock(&mutex_hotel);
     while(hotel.tot_occupancy < 2*N)
         pthread_cond_wait(&cond_occupancy, &mutex_hotel);
-    
-    //clean all the rooms
-    for(int i=0; i<N; i++)
-    {
-        if(hotel.rooms[i].occupancy == 2)
-        {
-            pthread_t target = evict(hotel, N, pthread_self(), 0, i);
-            pthread_kill(target, SIGUSR1);
-        }
-    }
-
     pthread_mutex_unlock(&mutex_hotel);
     // Cleaner apparently kicks out the guests at this point LOL
     cout<<"Hotel cleanup begins. Evicting current guests"<<endl;
