47a49ef87ab10b38385da68f930ef93d38c3f002
Kartik Pontula <kartikpontula@gmail.com>
2023-04-11 22:17:56 +0530

    Merged changes into main

--- A6/goodmalloc.cpp
+++ A6/goodmalloc.cpp
@@ -1,18 +1,54 @@
 #include "goodmalloc.hpp"
 
-void createMem()
+// Create contiguous memory segment to contain all linked lists.
+void GoodMallocMemory::createMem(size_t memsize)
+{
+    // do some malloc'ing into memory
+    return;
+}
+// Create doubly linked list inside contiguous memory.
+void GoodMallocMemory::createList(string listname, int listsize)
+{
+    // if mem not allocated then foff
+    return;
+}
+// Update element of an existing doubly linked list.
+void GoodMallocMemory::assignVal(string listname, int offset, int value)
 {
     return;
 }
-void createList()
+// Free the variables which are out-of-scope.
+void GoodMallocMemory::freeElem()
 {
     return;
 }
-void assignVal()
+// Free a particular variable.
+void GoodMallocMemory::freeElem(string listname)
 {
     return;
 }
-void freeElem()
+// Convert frame number to element pointer.
+Element* GoodMallocMemory::frameToPtr(int frameno)
+{
+    return (this->mem + frameno*FRAMESIZE);
+}
+// Get the frame number of the node at an offset.
+int GoodMallocMemory::getFrameNo(string listname, int offset)
+{
+    return;
+}
+// Assign to a node using the frame number +/- an offset.
+int GoodMallocMemory::setVal(int frameno, int offset, int value)
+{
+    return;
+}
+// To be called immediately after entering a scope.
+void GoodMallocMemory::enterScope(string func)
+{
+    return;
+}
+// To be called immediately before exiting a scope.
+void GoodMallocMemory::exitScope()
 {
     return;
 }
--- A6/goodmalloc.hpp
+++ A6/goodmalloc.hpp
@@ -2,10 +2,47 @@
 #define __GOODMALLOC_H_
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <map>
+using namespace std;
 
-void createMem();
-void createList();
-void assignVal();
-void freeElem();
+#define FRAMESIZE 16
+typedef struct _Element
+{
+    long int data : 64; // 8-byte integer element
+    int prev : 32; // 4-byte
+    int next : 32; // 4-byte
+} Element; // total size = 16 bytes
+
+typedef struct _PTEntry
+{
+    int head; // 4-byte
+    int tail; // 4-byte
+    int scope; // 4-byte
+} PTEntry; // total size = 12 bytes
+
+class GoodMallocMemory
+{
+    Element* mem; // allocated memory, interpreted as an array of element-sized frames
+    map<string, PTEntry> PT; // page table
+    string scope; // suffix used to scope list names
+    vector<int> freeFrames; // list of available frame indices
+    stack<string> varStack; // global variable stack
+
+    public:
+    void createMem(size_t memsize);
+    void createList(string listname, int listsize);
+    void assignVal(string listname, int offset, int value);
+    void freeElem();
+    void freeElem(string listname);
+    // TODO: add helper functions to append scope to variable names, parse them etc.
+    Element* frameToPtr(int frameno);
+    int getFrameNo(string listname, int offset);
+    int setVal(int frameno, int offset, int value);
+    void enterScope(string func);
+    void exitScope();
+};
 
 #endif
