bfdffbefb2325d32cd0d45f54c2b23e62198a88c
rahulmandalgg <rahulmandal912@gmail.com>
2023-04-13 12:34:36 +0530

    check this

--- A6/goodmalloc.cpp
+++ A6/goodmalloc.cpp
@@ -1,54 +1,232 @@
 #include "goodmalloc.hpp"
-
+// Constructor of the GoodMallocMemory class.
+GoodMallocMemory::GoodMallocMemory()
+{
+    mem = nullptr;
+    maxFrameCount = 0;
+    PT = map<string, PTEntry>();
+    scope = "";
+    freeFrameHead = -1;
+    freeFrameTail = -1;
+    freeFrameCount = 0;
+    varStack = stack<string>();
+}
 // Create contiguous memory segment to contain all linked lists.
 void GoodMallocMemory::createMem(size_t memsize)
 {
-    // do some malloc'ing into memory
+    if(mem) throw runtime_error("createMem: Only one memory block to be created per instance of GoodMallocMemory");
+    // allocate space in multiple of Element size
+    // Assumption: Element size is 2^4 bytes
+    maxFrameCount = (memsize>>4)+1;
+    mem = (Element*) calloc(maxFrameCount, FRAMESIZE);
+    if(!mem) throw runtime_error("createMem: Memory allocation failure");
+
+    // initialize memory with implicit free list
+    freeFrameHead = 0;
+    freeFrameTail = maxFrameCount-1;
+    freeFrameCount = maxFrameCount;
+    for(int i=0; i<maxFrameCount; i++)
+    {
+        mem[i].prev = i-1;
+        if(i==maxFrameCount-1) mem[i].next = -1;
+        else mem[i].next = i+1;
+    }
+    // return;
+}
+// Create a doubly linked list inside contiguous memory.
+void GoodMallocMemory::createList(string listname, size_t listlen)
+{
+    // Error reporting for illegal states
+    if(listname.find(" ")!=string::npos || listname.find("$")!=string::npos || listname.find("|")!=string::npos)
+        throw runtime_error("createList: Variable name must not contain ' ' or '$' or '|'");
+    if(!mem)
+        throw runtime_error("createList: Memory not allocated, call createMem() first");
+    if(freeFrameCount < listlen)
+        throw runtime_error("createList: Not enough free memory for the list");
+    if(listlen == 0)
+        throw runtime_error("createList: List size must be a positive integer");
+    
+    string absolute_name = scope + " | " + listname;
+    // traverse free list, allocate memory using First Fit
+    PTEntry newlist;
+    newlist.head = newlist.tail = freeFrameHead;
+    newlist.scope = 1;
+    for(size_t i=0; i<(listlen-1); i++)
+    {
+        if(mem[newlist.tail].next != -1) newlist.tail = mem[newlist.tail].next;
+        else throw runtime_error("createList: Went out of free list bounds");
+    }
+    // if all free frames used up
+    if(newlist.tail == freeFrameTail)
+    {
+        freeFrameHead = freeFrameTail = -1;
+    }
+    else
+    {
+        freeFrameHead = mem[newlist.tail].next;
+        mem[freeFrameHead].prev = -1;
+    }
+    mem[newlist.tail].next = -1;
+    freeFrameCount -= listlen;
+    // add list to page table
+    PT.insert(make_pair(absolute_name, newlist));
+    // push to varStack
+    varStack.push(absolute_name);
     return;
 }
-// Create doubly linked list inside contiguous memory.
-void GoodMallocMemory::createList(string listname, int listsize)
+// Update a specific element of a list. Worst case O(n) complexity.
+void GoodMallocMemory::assignVal(string listname, size_t offset, long value)
 {
-    // if mem not allocated then foff
-    return;
-}
-// Update element of an existing doubly linked list.
-void GoodMallocMemory::assignVal(string listname, int offset, int value)
-{
+    string absolute_name = scope + " | " + listname;
+    if(PT.find(absolute_name) == PT.end()) throw runtime_error("assignVal: List does not exist");
+    PTEntry& currlist = PT[absolute_name];
+    if(currlist.scope == 0) throw runtime_error("assignVal: List is out-of-scope, cannot assign to it");
+    // Traverse list and assign element
+    int target = currlist.head;
+    for(size_t i=0; i<offset; i++)
+    {
+        if(mem[target].next != -1) target = mem[target].next;
+        else throw runtime_error("assignVal: Offset is out of bounds");
+    }
+    mem[target].data = value;
     return;
 }
 // Free the variables which are out-of-scope.
 void GoodMallocMemory::freeElem()
 {
+    // go through entire PT for entries where scope == 0, and do free on them each
+    for(auto it = PT.begin(); it != PT.end();)
+    {
+        if(it->second.scope == 0) // remove each list which is out-of-scope
+        {
+            cout<<"Freeing list "<<it->first<<endl;
+            PTEntry& currlist = it->second;
+            // append this list to freeframes list
+            mem[freeFrameTail].next = currlist.head;
+            mem[currlist.head].prev = freeFrameTail;
+            freeFrameTail = currlist.tail;
+            // O(n) updation code for freeFrameCount
+            freeFrameCount = 0;
+            for(int f=freeFrameHead; f!=-1; f = mem[f].next)
+            {
+                freeFrameCount++;
+            }
+            // remove page table entry
+            it = PT.erase(it);
+        }
+        else it++;
+    }
     return;
 }
-// Free a particular variable.
+// Free a particular list.
 void GoodMallocMemory::freeElem(string listname)
 {
+    string absolute_name = scope + " | " + listname;
+    if(PT.find(absolute_name) == PT.end()) throw runtime_error("freeElem: List does not exist");
+    PTEntry& currlist = PT[absolute_name];
+    // append this list to freeframes list
+    mem[freeFrameTail].next = currlist.head;
+    mem[currlist.head].prev = freeFrameTail;
+    freeFrameTail = currlist.tail;
+    // O(n) updation code for freeFrameCount
+    freeFrameCount = 0;
+    for(int f=freeFrameHead; f!=-1; f = mem[f].next)
+    {
+        freeFrameCount++;
+    }
+    // remove page table entry
+    PT.erase(absolute_name);
     return;
 }
+// Print the contents of a list.
+void GoodMallocMemory::printList(string listname)
+{
+    string absolute_name = scope + " | " + listname;
+    if(PT.find(absolute_name) == PT.end()) throw runtime_error("printList: List does not exist");
+    PTEntry& currlist = PT[absolute_name];
+    if(currlist.scope == 0) throw runtime_error("printList: List is out-of-scope, cannot print it");
+    // Traverse list and print element
+    int target = currlist.head;
+    // int k=0;
+    while(target != -1)                     // remove k<50 to print entire list !!!!!!!!!!!!!!!!!!!!!!!!!!1
+    {
+        cout<<mem[target].data<<" "<<endl;
+        target = mem[target].next;
+        // k++;
+    }
+    cout<<endl;
+    return;
+}
+
 // Convert frame number to element pointer.
 Element* GoodMallocMemory::frameToPtr(int frameno)
 {
-    return (this->mem + frameno*FRAMESIZE);
+    return &mem[frameno];
 }
 // Get the frame number of the node at an offset.
-int GoodMallocMemory::getFrameNo(string listname, int offset)
+int GoodMallocMemory::getFrameNo(string listname, size_t offset)
 {
-    return;
+    string absolute_name = scope + " | " + listname;
+    if(PT.find(absolute_name) == PT.end()) throw runtime_error("getFrameNo: List does not exist");
+    PTEntry& currlist = PT[absolute_name];
+    if(currlist.scope == 0) throw runtime_error("getFrameNo: List is out-of-scope, cannot access it");
+    // Traverse list and get element
+    int target = currlist.head;
+    for(size_t i=0; i<offset; i++)
+    {
+        if(mem[target].next != -1) target = mem[target].next;
+        else throw runtime_error("getFrameNo: Offset is out of bounds");
+    }
+    return target;
 }
 // Assign to a node using the frame number +/- an offset.
-int GoodMallocMemory::setVal(int frameno, int offset, int value)
+int GoodMallocMemory::setVal(int frameno, int offset, long value)
 {
-    return;
+    int target = frameno;
+    for(int i=0; i<offset; i++)
+    {
+        if(mem[target].next != -1) target = mem[target].next;
+        else throw runtime_error("setVal: Offset is out of bounds");
+    }
+    mem[target].data = value;
+    return target;
 }
 // To be called immediately after entering a scope.
 void GoodMallocMemory::enterScope(string func)
 {
+    cout<<"Entering scope "<<func<<endl;
+    // prepend with current function name
+    scope = func + " " + scope;
+    // mark the stack with sentinel string $
+    varStack.push("$");
     return;
 }
 // To be called immediately before exiting a scope.
 void GoodMallocMemory::exitScope()
 {
+    // extract current scope prefix
+    string prefix = "";
+    size_t pos = scope.find(" "); // Assumption: scope doesn't begin with " "
+    if(pos != string::npos)
+    {
+        prefix = scope.substr(0, pos);
+        cout<<"Exiting scope "<<prefix<<endl;
+        scope.erase(0, pos+1);
+    }
+    else scope = "";
+    // pop from stack until $ is reached
+    while(!varStack.empty())
+    {
+        string curr = varStack.top();
+        varStack.pop();
+        if(curr == "$") break; // exit when no more variables to pop from current scope
+        if(curr.find(prefix) != 0) // report error if curr is not within scope
+            throw runtime_error("exitScope: Non-scope variable in stack");
+        // mark the PTEntry of curr as out-of-scope
+        if(PT.find(curr) != PT.end())
+            PT[curr].scope = 0;
+        // there may be stack variables which are absent from the page table, presumably removed using freeElem(name)
+        // else throw runtime_error("exitScope: Stack variable not present in page table");
+    }
     return;
 }
--- A6/mergesort.cpp
+++ A6/mergesort.cpp
@@ -3,33 +3,86 @@
 #include <random>
 using namespace std;
 
-#define CREATEMEM_SIZE (250 * 1024 * 1024)
-#define CREATELIST_SIZE 50000
-#define ELEMENT_MAX 100000
-
-GoodMallocMemory M1;
+GoodMallocMemory M;
 
 int mergeSort(int head)
 {
-    M1.enterScope(__func__);
-    
-    M1.exitScope();
+    M.enterScope(__func__);
+    // base case
+    if(M.frameToPtr(head)->next == -1)
+    {
+        M.exitScope();
+        return head;
+    }
+    // find middle element
+    int slow = head, fast = head;
+    while(M.frameToPtr(fast)->next != -1 && M.frameToPtr(M.frameToPtr(fast)->next)->next != -1)
+    {
+        slow = M.frameToPtr(slow)->next;
+        fast = M.frameToPtr(M.frameToPtr(fast)->next)->next;
+    }
+    int mid = M.frameToPtr(slow)->next;
+    // M.frameToPtr(slow)->next = -1;
+
+    // sort left and right halves
+    // int left = mergeSort(head);
+    // int right = mergeSort(mid);
+
+    // merge sorted halves
+    // int sortedHead = -1, sortedTail = -1;
+    // while(left != -1 && right != -1)
+    // {
+    //     if(M.frameToPtr(left)->data < M.frameToPtr(right)->data)
+    //     {
+    //         if(sortedHead == -1) sortedHead = left;
+    //         else M.frameToPtr(sortedTail)->next = left;
+    //         sortedTail = left;
+    //         left = M.frameToPtr(left)->next;
+    //     }
+    //     else
+    //     {
+    //         if(sortedHead == -1) sortedHead = right;
+    //         else M.frameToPtr(sortedTail)->next = right;
+    //         sortedTail = right;
+    //         right = M.frameToPtr(right)->next;
+    //     }
+    // }
+    // if(left != -1)
+    // {
+    //     if(sortedHead == -1) sortedHead = left;
+    //     else M.frameToPtr(sortedTail)->next = left;
+    // }
+    // if(right != -1)
+    // {
+    //     if(sortedHead == -1) sortedHead = right;
+    //     else M.frameToPtr(sortedTail)->next = right;
+    // }
+
+
+    cout<<M.frameToPtr(head)->data<<" "<<M.frameToPtr(mid)->data<<endl;
+
+    M.exitScope();
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
-    M1.enterScope(__func__);
-    M1.createMem(CREATEMEM_SIZE);
-    M1.createList("mylist", CREATELIST_SIZE);
+    M.enterScope(__func__);
+    M.createMem(250 * 1024 * 1024);
+    M.createList("mylist", 10);
     // assign random elements to list
     random_device rd; mt19937 gen(rd());
-    uniform_int_distribution<> dist(1, ELEMENT_MAX);
-    for(int i=0; i<CREATELIST_SIZE; i++) // optional TODO: improve efficiency
+    uniform_int_distribution<> dist(1, 100000);
+    for(int i=0; i<10; i++) // optional TODO: improve efficiency
     {
-        M1.assignVal("mylist", i, dist(gen));
+        M.assignVal("mylist", i, dist(gen));
     }
+    
     // perform merge sort
-    int sortedHead = mergeSort(M1.getFrameNo("mylist", 0));
+    int sortedHead = mergeSort(M.getFrameNo("mylist", 0));
+    // print sorted list
+    M.printList("mylist");
+    M.exitScope();
     
     return 0;
 }
